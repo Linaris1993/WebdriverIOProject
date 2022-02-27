6501d39c8f1013cb4df96a8fbb25aab2f0f3f5bb
Linaris1993 <linakhatmullina@gmail.com>
2022-02-27 16:12:16 -0500

    added attributeTest

---
+++ test/specs/attributePage.spec.js
@@ -0,0 +1,32 @@
+const mainPage = require('../pageobjects/mainPage');
+const attributePage = require('../pageobjects/attributePage');
+
+describe('Class Attribute Page', () => {
+
+    const attributePageH3 = 'Class Attribute'
+    beforeEach(() => {
+        browser.maximizeWindow();
+        browser.deleteAllCookies();
+        browser.refresh();
+    })
+
+    it('Clicking on "Attribute" link redirects me to appropriate page', async () => {
+        await attributePage.open();
+
+        await expect(attributePage.attributeLink).toBeExisting();
+        attributePage.attributeLink.click();
+        await expect(mainPage.h3).toHaveTextContaining(attributePageH3);
+        await expect(browser).toHaveUrl('http://uitestingplayground.com/classattr');
+    });
+
+    it('Attribute scenario', async() => {
+        await attributePage.blueBtn.waitForDisplayed();
+        // const elem = await attributePage.blueBtn
+        // const color = await elem.getCSSProperty('color')
+        // console.log(color) //need to find btn by color 
+        // const alertText = await browser.getAlertText();
+        // await browser.acceptAlert();
+        // await expect(alertText).toHaveText('Primary button pressed');
+      });
+
+});
